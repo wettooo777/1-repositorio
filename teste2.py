fff9ec89b8b70b6904be8960f0466e53aee52815
wettooo777 <welliton.souza.oliveira@escola.pr.gov.br>
2025-05-22 14:51:42 +0000

    fiz um algoritmo de "se maior com votos digitaveís"

---
+++ teste2.py
@@ -0,0 +1,10 @@
+curso01 = int(input("quantos votos tem o curso01:"))
+curso02 = int(input("quantos votos tem o curso02:"))
+
+
+if curso02 < curso01:
+    print("curso01 é  maior")
+elif curso02 > curso01:
+    print("curso02 é maior")
+else:
+    print("empate") 
